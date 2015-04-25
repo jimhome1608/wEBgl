b3ce18237b6c61bc5e699c27268c204a1681d358
Jim Home <jimhome1608@gamil.com>
2015-04-25 21:00:17 +1000

    Added Cube

--- HelloPoint1.js
+++ HelloPoint1.js
@@ -2,13 +2,13 @@
 // Vertex shader program 
 var VSHADER_SOURCE = 
     'void main() {\n' + 
-      ' gl_Position = vec4( 0.0, 0.0, 0.0,1.0);\n' + // Coordinates 
+      ' gl_Position = vec4( 0.5, 0.5, 0.0,1.0);\n' + // Coordinates 
       ' gl_PointSize = 10.0;\n' + // Set the point size  
     '}\n'; 
     // Fragment shader program 
 var FSHADER_SOURCE = 
         'void main() {\n' + 
-            ' gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0);\n' + // Set the color 
+            ' gl_FragColor = vec4( 0.0, 1.0, 0.0, 1.0);\n' + // Set the color 
         '}\n'; 
 function main() { 
     // Retrieve < canvas > element 
